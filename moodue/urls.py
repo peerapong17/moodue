335c0666ad2ecdc3d66acf81e64d5f1667e040b0
peerapong17 <17peerapong4.0@gmail.com>
2021-12-10 19:24:19 +0700

    dsa

--- moodue/urls.py
+++ moodue/urls.py
@@ -23,8 +23,7 @@ from django.conf.urls.static import static
 from django.views.static import serve
 
 urlpatterns = [
-    path("", lambda request: redirect('home/', permanent=False)),
     path('admin/', admin.site.urls),
-    path("home/", include('home.urls')),
+    path("/", include('home.urls')),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
