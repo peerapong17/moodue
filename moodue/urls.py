1a2cd2fe925d56436f8bead41cc5453557444022
peerapong17 <17peerapong4.0@gmail.com>
2021-12-10 17:42:43 +0700

    second commit

--- moodue/urls.py
+++ moodue/urls.py
@@ -1,4 +1,4 @@
-"""soytech URL Configuration
+"""moodue URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -15,13 +15,18 @@ Including another URLconf
 """
 
 from django.conf import settings
+from django.conf.urls import url
 from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path("", lambda request: redirect('home/', permanent=False)),
     path('admin/', admin.site.urls),
     path("home/", include('home.urls')),
+    
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- moodue/wsgi.py
+++ moodue/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for soytech project.
+WSGI config for moodue project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'soytech.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moodue.settings')
 
 application = get_wsgi_application()
